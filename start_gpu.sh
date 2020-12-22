333c2109c095cfdf17c9093115060ce68fc792e0
bhpchen <billy.chen@robot-data.ai>
2020-12-22 11:08:45 +0800

    @update

--- start_gpu.sh
+++ start_gpu.sh
@@ -1,3 +1,3 @@
 docker rm tf_jl
 docker build -t tensorflow_jupyterlab .
-docker run --gpus all -p 8888:8888 -v $(pwd):/workspace/src/ --name tf-jl tensorflow_jupyterlab
+docker run --gpus all -p 8888:8888 -v nfs_nas:/workspace/src/ --name tf-jl tensorflow_jupyterlab
